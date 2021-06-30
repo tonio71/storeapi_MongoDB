f8a32e240f5d3cd01874a3121f5ef1b01b99a648
tonio71 <tonio.maria@gmail.com>
2021-06-30 19:13:57 -0300

    Update db.js

--- repositories/db.js
+++ repositories/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-  "postgres://fljnyomj:3_kxlO9sfEJLqSKerr596V0hVp68Sjye@motty.db.elephantsql.com/fljnyomj",
+  "postgres://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   {
     dialect:"postgres",
     define:{
