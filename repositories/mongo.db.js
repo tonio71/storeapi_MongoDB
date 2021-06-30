a52735bda0819fa8d756ce29e4ea5a6dce98a71e
tonio71 <tonio.maria@gmail.com>
2021-06-30 19:14:49 -0300

    Update mongo.db.js

--- repositories/mongo.db.js
+++ repositories/mongo.db.js
@@ -3,7 +3,7 @@ import mongodb from "mongodb";
 function getClient(){
 
     //console.log("fazendo conexao...")
-    const uri = "mongodb+srv://tonio:tonio@cluster0.i7qu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
     const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     //console.log("conexao feita... ",client);
     return client;
